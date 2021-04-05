0054807dff388eb820a93dc5821db8033acbb7c6
Yom-gi <guillaume.pelette@protonmail.com>
2021-04-05 17:17:59 +0200

    modified Form PostingType

--- src/Form/PostingType.php
+++ src/Form/PostingType.php
@@ -4,31 +4,105 @@ namespace App\Form;
 
 use App\Entity\Posting;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class PostingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('adresse')
-            ->add('type')
-            ->add('meuble')
-            ->add('nombre_pieces')
-            ->add('superficie')
-            ->add('loyer')
-            ->add('caution')
-            ->add('date_debut')
-            ->add('duree_bail')
-            ->add('cave')
-            ->add('place_parking')
-            ->add('ascenseur')
-            ->add('gardien')
-            ->add('balcon')
-            ->add('cuisine_equipee')
-            ->add('toilette_separee')
-            ->add('photo1')
+            ->add('adresse', TextType::class)
+            ->add('type', ChoiceType::class)
+            ->add('meuble', ChoiceType::class)
+            ->add('nombre_pieces', ChoiceType::class)
+            ->add('superficie', DateType::class)
+            ->add('loyer', NumberType::class)
+            ->add('caution', NumberType::class)
+            ->add('date_debut', DateType::class)
+            ->add('duree_bail', NumberType::class)
+            ->add('cave', ChoiceType::class)
+            ->add('place_parking', ChoiceType::class)
+            ->add('ascenseur', ChoiceType::class)
+            ->add('gardien', ChoiceType::class)
+            ->add('balcon', ChoiceType::class)
+            ->add('cuisine_equipee', ChoiceType::class)
+            ->add('toilette_separee', ChoiceType::class)
+            ->add('photo1', FileType::class, [
+                'data_class' => null,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                        ]
+                    ])
+                ]
+            ])
+            ->add('photo2', FileType::class,[
+                'data_class' => null,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                        ]
+                    ])
+                ]
+            ])
+            ->add('photo3', FileType::class, [
+                'data_class' => null,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                        ]
+                    ])
+                ]
+            ])
+            ->add('photo4', FileType::class,[
+                'data_class' => null,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                        ]
+                    ])
+                ]
+            ])
+            ->add('photo5', FileType::class, [
+                'data_class' => null,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                        ]
+                    ])
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Créer l\'annonce',
+            ])
         ;
     }
 
